bd5af7b3495287e9bd88506c309764d37ac87bd1
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-05-14 10:21:32 +0000

    pure python example continued

--- experimental/pure.py
+++ experimental/pure.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 def empty_account():
     return ([], [])
 
@@ -12,8 +15,10 @@ def credit(account, amount):
 
 def process_entry(ledger, entry):
     dr, cr, amount = entry
-    ledger[dr] = debit(ledger[dr], amount)
-    ledger[cr] = credit(ledger[cr], amount)
+    _ledger = deepcopy(ledger)
+    _ledger[dr] = debit(ledger[dr], amount)
+    _ledger[cr] = credit(ledger[cr], amount)
+    return _ledger
 
 
 def subset(chart, account_types):
@@ -51,24 +56,31 @@ def make_ledger(chart):
     }
 
 
-def trim_ledger(chart, ledger, filter_func):
+def get_balances(chart, ledger):
     return {
         account_name: balance(chart, account_name, account)
         for account_name, account in ledger.items()
+    }
+
+
+def filter_balances(chart, balances, filter_func):
+    return {
+        account_name: balance
+        for account_name, balance in balances.items()
         if filter_func(chart, account_name)
     }
 
 
-def debit_account_balances(chart, ledger):
-    return trim_ledger(chart, ledger, is_debit_account)
+def trial_balance_as_tuple(chart, ledger):
+    trial_balance = get_balances(chart, ledger)
+    return (
+        filter_balances(chart, trial_balance, is_debit_account),
+        filter_balances(chart, trial_balance, is_credit_account),
+    )
 
 
-def credit_account_balances(chart, ledger):
-    return trim_ledger(chart, ledger, is_credit_account)
-
-
-def trial_balance(chart, ledger):
-    return debit_account_balances(chart, ledger), credit_account_balances(chart, ledger)
+def union(d1, d2):
+    return {**d1, **d2}
 
 
 def sum_dict(d):
@@ -87,47 +99,82 @@ ledger_ = make_ledger(chart_)
 entries = [
     ("cash", "equity", 299),
     ("cash", "equity", 301),
-    ("cash", "services", 1500),
+    ("cash", "services", 870),
     ("payroll", "cash", 355),
     ("rent", "cash", 200),
+    ("cash", "loan", 500),
+    ("interest", "cash", 15),
 ]
 for entry in entries:
-    process_entry(ledger_, entry)
+    ledger_ = process_entry(ledger_, entry)
 print(ledger_)
-a, b = trial_balance(chart_, ledger_)
+a, b = trial_balance_as_tuple(chart_, ledger_)
 print(a)
 print(sum_dict(a))
 print(b)
 print(sum_dict(b))
 assert sum_dict(a) == sum_dict(b)
-print({**a, **b})
+print(union(a, b))
 # balance, pl
+
 
 def pick(chart, ledger, keys):
     res = {}
-    tb1, tb2 = trial_balance(chart, ledger)
-    tb = {**tb1, **tb2}
+    trial_balance = get_balances(chart, ledger)
     for key in keys:
         res[key] = {}
         for account_name in chart[key]:
-            res[key][account_name] = tb[account_name]
+            res[key][account_name] = trial_balance[account_name]
     return res
+
 
 def income_statement(chart, ledger):
-    res = pick(chart, ledger, "income expenses".split())
-    res["profit"] = sum_dict(res["income"]) - sum_dict(res["expenses"])
+    res = pick(chart, ledger, ["income", "expenses"])
+    res["profit"] = {"profit": sum_dict(res["income"]) - sum_dict(res["expenses"])}
     return res
 
+
 def current_profit(chart, ledger):
-    return income_statement(chart, ledger)["profit"]
+    return income_statement(chart, ledger)["profit"]["profit"]
+
 
 def close(chart, ledger, re):
+    # this is a hack to produce quick view of balance
+    # this a signle, not a double entry posting
     ledger[re] = credit(ledger[re], current_profit(chart, ledger))
     return ledger
 
+
 def balance_sheet(chart, ledger):
     close(chart, ledger, "retained_earnings")
-    return pick(chart, ledger, "assets capital liabilities".split())
+    return pick(chart, ledger, ["assets", "capital", "liabilities"])
 
-print(income_statement(chart_, ledger_))
-print(balance_sheet(chart_, ledger_))
+
+inc = income_statement(chart_, ledger_)
+bal = balance_sheet(chart_, ledger_)
+
+from pprint import pprint
+
+pprint(inc)
+pprint(bal)
+
+
+def to_line(s):
+    return s.replace("_", " ").capitalize()
+
+
+def to_number(x):
+    return str(x).rjust(5, " ")
+
+
+def print_statement(report, title=""):
+    print()
+    print(title)
+    for key, xs in report.items():
+        print(to_line(key).ljust(25, "."), to_number(sum_dict(xs)))
+        for account_name, amount in xs.items():
+            print(("  " + to_line(account_name)).ljust(25, "."), to_number(amount))
+
+
+print_statement(inc, "Income statement")
+print_statement(bal, "Balance sheet")
