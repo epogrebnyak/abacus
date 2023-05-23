0b78d703b4f6c7c8174626f394cc648142a68ebe
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-05-23 07:37:05 +0000

    test: comment retained earnigns

--- tests/test_chart.py
+++ tests/test_chart.py
@@ -70,9 +70,9 @@ def test_chart_flat():
     assert chart.set_retained_earnings_account("retained_earnings")._flat() == [
         (Asset, ["cash", "receivables", "goods_for_sale", "ppe"]),
         (Expense, ["cogs", "sga", "depreciation_expense"]),
-        (Capital, ["equity"]),
+        (Capital, ["equity"]), # no retained_earnings here 
         (Liability, ["dividend_due", "payables"]),
         (Income, ["sales"]),
         (IncomeSummaryAccount, ["_profit"]),
-        (RetainedEarnings, ["retained_earnings"]),
+        (RetainedEarnings, ["retained_earnings"]), # retained_earnings at the end
     ]
