74a97a56f7d1b5fb7dea44282e1c38aa51d711ba
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-05-24 22:31:11 +0000

    Possible cli (for target release 0.4.0) #19

--- experimental/cli.py
+++ experimental/cli.py
@@ -1,31 +1,35 @@
 """Abacus.
 
 Usage:
-  cli.py init chart
+  cli.py create chart [--output=<filename>]
+  cli.py create balances --chart=<filename1> [--output=<filename2>]
+  cli.py create store --chart=<filename1> [--start-balances=<filename2>] [--output=<filename3>]
   cli.py config (set | remove) --chart=<filename>
   cli.py config (set | remove) --store=<filename>
-  cli.py add account <name> (--asset | --expense | --capital | --retained-earnings | --liability | --income) [--title=<title>] --chart=<filename>
-  cli.py add account <name> --isa [--title=<title>] --chart=<filename>
+  cli.py add account <name> (--asset | --expense | --capital | --liability | --income) [--title=<title>] --chart=<filename>
   cli.py add accounts <names> (--asset | --expense | --capital | --liability | --income) --chart=<filename>
+  cli.py set retained-earnings-account <name> [--title=<title>] --chart=<filename>
+  cli.py set income-summary-account <name> [--title=<title>] --chart=<filename>
   cli.py add contra-account <name> --account=<name> --resulting-name=<resulting_name> [--resulting-title=<resulting_title>] --chart=<filename>
   cli.py add contra-accounts <names> --account=<name> --resulting-name=<resulting_name> --chart=<filename>
-  cli.py init balances --chart=<filename>
-  cli.py init store --chart=<filename1> [--balances=<filename2>]
   cli.py verify chart --chart=<filename>
   cli.py verify store --store=<filename>
   cli.py add entry <dr_account> <cr_account> <amount> [--title=<title>] --store=<filename> [--adjustment | --post-close]
-  cli.py add entry --dr=<dr_account> --cr=<cr_account> --amount=<amount> [--title=<title>] --store=<filename> [--adjustment | --post-close]
+  cli.py add entry --dr=<dr_account> --cr=<cr_account> --amount=<amount> [--date=<date>] [--title=<title>] --store=<filename> [--adjustment | --post-close]
   cli.py mark --business-end --store=<filename>
   cli.py tb [--credit | --debit] [--sum] --store=<filename>
+  cli.py net --all --store=<filename>
   cli.py net contra-accounts (--asset | --expense | --capital | --liability | --income | --permanent | --temporary | --all) --store=<filename>
   cli.py close (--all | --income | --expense | --isa) --store=<filename>
   cli.py report --income-balance --store=<filename> [--console]
   cli.py report --balance-sheet --store=<filename> [--console]
-  cli.py show chart --store=<filename>
-  cli.py show entries --store=<filename>
-  cli.py show ledger --store=<filename>
-  cli.py show balances --store=<filename>
-  cli.py freeze --store=<filename>
+  cli.py show chart --store=<filename> [--console]
+  cli.py show entries --store=<filename> [--console]
+  cli.py show ledger --store=<filename> [--console]
+  cli.py show balances --store=<filename> [--console]
+  cli.py freeze --store=<filename1> [--output=<filename2>]
+  cli.py save --store=<filename1> --to-excel=<filename2> [--overwrite]
+  cli.py load --store=<filename1> --from-excel=<filename2> [--overwrite]
   cli.py (-h | --help)
   cli.py --version
 
