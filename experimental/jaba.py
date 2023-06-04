cc5fc0c29537593e3ff8ae2dbde638e0a31b473c
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-06-04 19:38:40 +0300

    #19: jaba.py has docopt prototype for CLI (not mounted to endpoint)

--- experimental/jaba.py
+++ experimental/jaba.py
@@ -2,7 +2,7 @@
 `jaba` is a minimal accounting manager.
 
 Usage:
-  jaba chart <chart_file> touch
+  jaba chart <chart_file> create
   jaba chart <chart_file> add assets <account_names>...
   jaba chart <chart_file> add expenses <account_names>...  
   jaba chart <chart_file> add capital <account_names>...
@@ -11,27 +11,28 @@ Usage:
   jaba chart <chart_file> add income <account_names>...   
   jaba chart <chart_file> offset <name> <new_name> <account_names>...  
   jaba chart <chart_file> list
-  jaba names <name_file> touch
+  jaba store <store_file> create --chart <chart_file> [--start start_balances.json]
+  jaba store <store_file> post <dr_account> <cr_account> <amount>
+  jaba store <store_file> close --all
+  jaba store <store_file> list [--head <n> | --tail <n> | --last ]
+  jaba store <store_file> list [--open | --usual | --adjust | --close | --post-close]
+  jaba store <store_file> report (-i | --income-statement) [--names <name_file>] 
+  jaba store <store_file> report (-b | --balance-sheet) [--names <name_file>] 
+  jaba store <store_file> balances [--credit | --debit] [--sum]
+  jaba store <store_file> balance <account_name>
+  jaba names <name_file> create
   jaba names <name_file> add <account_name> <title> 
   jaba names <name_file> list
-  jaba store <store_file> create [--init start_balances.json [--use-chart chart.json]]
-  jaba store <store_file> post <dr_account> <cr_account> <amount>
-  jaba store <store_file> list [--head <n> | --tail <n> | --last ])
-  jaba store <store_file> list [--from <event1>] [--to <event2>]
-  jaba store <store_file> mark <event>
-  jaba store <store_file> <chart_file> close (--contra-income --contra-expenses)
-  jaba store <store_file> <chart_file> close (--income --expenses) 
-  jaba store <store_file> <chart_file> close (--isa | --income-summary-account)
-  jaba store <store_file> <chart_file> close --all
-  jaba store <store_file> <chart_file> report (-i | --income-statement) [--names <name_file>] 
-  jaba store <store_file> <chart_file> report (-b | --balance-sheet) [--names <name_file>] 
-  jaba store <store_file> <chart_file> balances [--credit | --debit]
-  jaba store <store_file> <chart_file> balance <account_name>
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
 """
+
+# jaba store <store_file> <chart_file> close (--contra-income --contra-expenses)
+# jaba store <store_file> <chart_file> close (--income --expenses)
+# jaba store <store_file> <chart_file> close (--isa | --income-summary-account)
+
 
 from docopt import docopt
 
