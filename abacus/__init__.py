e0e4e9588de922d6c1b4c0d638cf611bb282ce1f
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-06-04 09:02:44 +0300

    removed store and pipeline

--- abacus/__init__.py
+++ abacus/__init__.py
@@ -21,6 +21,5 @@ Simplifying assumptions:
 from .accounting_types import Entry  # noqa: F401
 from .chart import Chart  # noqa: F401
 from .ledger import Ledger  # noqa: F401
-from .pipeline import Pipeline  # noqa: F401
 from .reports import BalanceSheet, IncomeStatement  # noqa: F401
 from .tables import PlainTextViewer, RichViewer  # noqa: F401
