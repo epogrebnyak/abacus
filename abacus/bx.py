e7497ea5d834d43c9e4c495b600f836862c62455
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-06-25 19:05:10 +0300

    added strong validate to 'bx ledger start'

--- abacus/bx.py
+++ abacus/bx.py
@@ -185,6 +185,7 @@ def ledger_command(arguments, directory=cwd()):
     ledger_path = Location(directory).entries
     if arguments["start"]:
         chart, _ = load_chart(directory)
+        chart.strong_validate()
         if arguments["--file"]:
             starting_balances = read_json(arguments["<balances_file>"])
         else:
@@ -239,12 +240,15 @@ def report_command(arguments, directory=cwd()):
 def account_info(account_name: str, amount: Amount):
     return f"account <{account_name}> balance is {amount}."
 
+def human_name(account):
+    return account.__class__.__name__
+    # must return 'Сontra income' and 'Retained earnings' 
 
 def print_account_balance(account_name: str, directory=cwd()):
     path = Location(directory).entries
     account = safe_load(Book, path).ledger()[account_name]
     print("Account", account_name)
-    print("  Account type:", account.__class__.__name__)
+    print("  Account type:", human_name(account))
     print("  Debits:", account.debits)
     print("  Credits:", account.credits)
     print("  Balance:", account.balance())
