79bc871d269198f13a659b6de6a810c5578f4058
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-09-08 06:43:45 +0000

    add TODO for migrations

--- experimental/tests/conftest.py
+++ experimental/tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest  # pylint: disable=import-error
 from engine.base import Entry
 from engine.chart import Chart
 from engine.ledger import Ledger
@@ -24,3 +25,13 @@ def ledger(chart):
             Entry("refunds", "ar", 50),
         ]
     )
+
+
+@pytest.fixture
+def entries():
+    e1 = Entry(debit="cash", credit="equity", amount=1000)
+    e2 = Entry(debit="goods", credit="cash", amount=250)
+    e3 = Entry(debit="cogs", credit="goods", amount=200)
+    e4 = Entry(debit="cash", credit="sales", amount=400)
+    e5 = Entry(debit="sga", credit="cash", amount=50)
+    return [e1, e2, e3, e4, e5]
--- tests/test_report.py
+++ tests/test_report.py
@@ -1,13 +1,13 @@
 from abacus.reports import BalanceSheet, IncomeStatement
 
-
+# TODO Migrate to experimental/tests/test_report.py
 def test_income_statement(chart0, entries0):
     income_st = chart0.book().post_many(entries0).close().income_statement()
     assert income_st == IncomeStatement(
         income={"sales": 400}, expenses={"cogs": 200, "sga": 50}
     )
 
-
+# TODO Migrate to experimental/tests/test_report.py
 def test_balance_sheet_with_close(chart0, entries0):
     balance_st = chart0.book().post_many(entries0).close().balance_sheet()
     assert balance_st == BalanceSheet(
@@ -16,7 +16,7 @@ def test_balance_sheet_with_close(chart0, entries0):
         liabilities={"divp": 0, "payables": 0},
     )
 
-
+# TODO Migrate to experimental/tests/test_report.py
 def test_balances(chart0, entries0):
     b = chart0.book().post_many(entries0).close().balances()
     assert b == {
