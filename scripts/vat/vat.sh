cccfb86defcbf8b1cb233d256205d9dbc12116db
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-11-05 01:37:04 +0300

    vat example for #56

---
+++ scripts/vat/vat.sh
@@ -0,0 +1,6 @@
+cx init
+abacus chart add --asset cash
+abacus chart add --income sales
+abacus chart add --liability vat
+abacus ledger post-compound --debit cash 120 --credit sales 100 --credit vat 20
+abacus report report --trial-balance
